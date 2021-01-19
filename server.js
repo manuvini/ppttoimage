91447b08905f25ed22a53053ed6cbef48a79ff72
Manoj D <manuvini29@gmail.com>
2021-01-19 10:28:54 +0530

    mm

---
+++ server.js
@@ -0,0 +1,43 @@
+import express from'express'; // requre the express framework
+import Converter from 'ppt-png';
+import {
+    fileExists
+} from '@hckrnews/converter';
+
+
+var app = express();
+
+// Create a server to listen at port 8080
+var server = app.listen(8080, function(){
+    var host = server.address().address
+    var port = server.address().port
+    console.log("REST API demo app listening at http://%s:%s", host, port)
+})
+
+
+const converter = Converter.create({
+    files:  ['input/sample.ppt'],
+    output: 'output/',
+    outputType: 'jpeg'
+});
+
+
+
+
+
+
+
+
+// Endpoint to Get a list of users
+app.get('/getUsers', function(req, res){
+    console.log("get user");
+    const result = converter.convert();
+    console.log({
+        exists: fileExists('output/sample.pdf'),
+        result
+    });
+    res.end("{ok}");
+})
+
+
+
