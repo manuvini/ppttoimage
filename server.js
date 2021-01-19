5f5da8c3c0fde6470d486d37920ebd6eea5baf67
Manoj D <manuvini29@gmail.com>
2021-01-19 20:14:51 +0530

    This is a new commit for what I originally planned to be amended

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var  express  = require('express'); // requre the express framework
-
+const AWS = require('aws-sdk');
 var fileupload = require('express-fileupload');
 var ppt2png = require('ppt2png');
+const fs = require('fs');
+const AWS = require('aws-sdk');
 
 var app = express();
 
@@ -14,7 +16,19 @@ var server = app.listen(8080, function(){
     console.log("REST API demo app listening at http://%s:%s", host, port)
 })
 
+// Enter copied or downloaded access ID and secret key here
+const ID = 'AKIAJXDHPGGMJ3Q6EPJA';
+const SECRET = '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';
 
+// The name of the bucket that you have created
+const BUCKET_NAME = 'splodeit';
+
+
+
+const s3 = new AWS.S3({
+    accessKeyId: ID,
+    secretAccessKey: SECRET
+});
 
 
 
@@ -28,17 +42,50 @@ var server = app.listen(8080, function(){
 app.post('/upload', function(req, res, next){
     console.log(req.files);
     const file = req.files.ppt;
+    const fname = file.name.split('.').slice(0, -1).join('.');
     file.mv ('./input/1' + file.name, function(err,result){
 
     });
-    ppt2png('./input/1' + file.name, './output/' + file.name, function( err ){
+    ppt2png('./input/1' + file.name, './output/' + fname, function( err ){
         if(err) {
             console.log(err);
         } else {
             console.log('convert successful.');
+            let links = [];
+            for(var i=0; i< 20; i++){
+                const path = './output/' +fname + '-'+ i+'.png';
+                console.log(path);
+                if (fs.existsSync(path)) {
+                    //file exists
+                    const fileContent = fs.readFileSync(file);
+                    const params = {
+                        Bucket: BUCKET_NAME,
+                        Key: file.name,
+                        Body: fileContent
+                        // CreateBucketConfiguration: {
+                        //     // Set your region here
+                        //     LocationConstraint: "us-east-2"
+                        // }
+                    };
+
+                    s3.upload(params, function(err, data) {
+                        if (err) console.log(err, err.stack);
+                        console.log('file uploaded ' + file.name + "   " + data.Location);
+                        links.push(data.Location);
+                    });
+                }else{
+                    i = 21;
+                }
+                res.end(links);
+                links = [];
+
+
+
+            }
+        
         }
     });    
-    res.end("{ok}");
+    
 })
 
 
