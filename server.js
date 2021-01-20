e2cbcde177c398006c3d86345de29ab927e7dc8d
Manoj D <manuvini29@gmail.com>
2021-01-20 15:30:16 +0530

    This is a new commit for what I originally planned to be amended

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 var  express  = require('express'); // requre the express framework
-const AWS = require('aws-sdk');
-var fileupload = require('express-fileupload');
 var ppt2png = require('ppt2png');
 const fs = require('fs');
-const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require('constants');
-
 var app = express();
 
-app.use(fileupload());
 
 // Create a server to listen at port 8080
 var server = app.listen(8080, function(){
@@ -17,29 +12,7 @@ var server = app.listen(8080, function(){
 })
 
 // Enter copied or downloaded access ID and secret key here
-const ID = 'AKIAJXDHPGGMJ3Q6EPJA';
-const SECRET = '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';
-
-// The name of the bucket that you have created
-const BUCKET_NAME = 'splodeit';
-
-
-
-const s3 = new AWS.S3({
-    accessKeyId: ID,
-    secretAccessKey: SECRET,
-    Bucket: BUCKET_NAME
-});
-
-let links =[];
-let paths = [];
-let params = [];
-
-
-
-
-
-
+let links = new Map();
 // Endpoint to Get a list of users
 app.post('/upload',async function(req, res, next){
     console.log(req.files);
@@ -53,23 +26,16 @@ app.post('/upload',async function(req, res, next){
             console.log(err);
         } else {
             console.log('convert successful.');
-            var i = 0;
-            for(i=0; i< 5; i++){
-                const path = './output/' +fname + '-'+ i+'.png';
+            for(i=0; i< 20; i++){
+                const path = 'http://52.14.131.94/' +fname + '-'+ i+'.png';
                 console.log(path);
                 if (fs.existsSync(path)) {
-                    paths.push(path);                        
+                    links.set(i,path);                       
                 }else{
-                    i = 6;
+                    i = 21;
                 }
             }
-            testgg();
-            
-           
-            res.end( "  mhjgjyhg");
-            console.log(links);
-            links = [];
-            
+            res.end(JSON.stringify(links));
         }
     });    
 
@@ -77,56 +43,4 @@ app.post('/upload',async function(req, res, next){
     
 })
 
-async function testgg(){
-    console.log('kdf');
-    for(var k =0; k<paths.length; k++){
-        const rep = await Promise.all(
-            fs.readFile(path[k], (err, data) => {
-                const para = {
-                    Bucket: BUCKET_NAME, // pass your bucket name
-                    Key: fname + '-'+ k+'.png', // file will be saved as testBucket/contacts.csv
-                    Body: data,
-                    'ACL': 'public-read'
-                };
-                params.push(para)
-                console.log("hello");
-            })
-        );
-           
-        
-        
-    }
 
-    const responses = await Promise.all(
-        params.map(param => s3.upload(param, function(s3Err, data) {
-            console.log('hey u');
-            if (s3Err) throw s3Err
-            console.log(`File uploaded successfully at ${data.Location}`)
-            links.push(data.Location);
-        }).promise())
-    )
-}
-
-// fs.readFileAsync = function fupload(path,i) {
-//     return new Promise(function(resolve, reject) {
-//     console.log('broo kkhv' +i +path);
-    
-//     fs.readFile(path, (err, data) => {
-//         console.log('bro jvhkg' +i +path);
-//         if (err) throw err;
-//         const params = {
-//             Bucket: BUCKET_NAME, // pass your bucket name
-//             Key: fname + '-'+ i+'.png', // file will be saved as testBucket/contacts.csv
-//             Body: data,
-//             'ACL': 'public-read'
-//         };
-//         s3.upload(params, function(s3Err, data) {
-//             console.log('hey u');
-//             if (s3Err) throw s3Err
-//             console.log(`File uploaded successfully at ${data.Location}`)
-//             links.push(data.Location);
-//         });
-//      });
-//     });
-    
-// }
